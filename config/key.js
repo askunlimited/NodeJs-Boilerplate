b2ee2a9bfa5c946b7ecea01d0643622006e2a902
efactor <efactoronline@gmail.com>
2020-04-13 09:10:51 +0100

    First Commit

---
+++ config/key.js
@@ -0,0 +1,3 @@
+module.exports = {
+    MongoURI: 'mongodb+srv://rootman:efa9779ask@datacluster-5i6jh.mongodb.net/test?retryWrites=true&w=majority'
+}
---
+++ models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserScehma = new mongoose.Schema ({
+    name: {
+        type: String,
+        required: false
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+const User = mongoose.model('User', UserScehma);
+
+module.exports = User;
